50a1bc29fb96dc4da917a47d73c52b3a138a2204
Tony Seebregts <tony.seebregts@gmail.com>
2020-08-07 10:47:35 -0700

    Removed references to 'twystd'

--- commands/const_darwin.go
+++ commands/const_darwin.go
@@ -1,12 +1,12 @@
 package commands
 
 const (
-	DEFAULT_WORKDIR     = "/usr/local/var/com.github.twystd.uhppoted"
-	DEFAULT_KEYSDIR     = "/usr/local/etc/com.github.twystd.uhppoted/acl/keys"
-	DEFAULT_KEYFILE     = "/usr/local/etc/com.github.twystd.uhppoted/acl/keys/uhppoted"
+	DEFAULT_WORKDIR     = "/usr/local/var/com.github.uhppoted"
+	DEFAULT_KEYSDIR     = "/usr/local/etc/com.github.uhppoted/acl/keys"
+	DEFAULT_KEYFILE     = "/usr/local/etc/com.github.uhppoted/acl/keys/uhppoted"
 	DEFAULT_CREDENTIALS = ""
 	DEFAULT_PROFILE     = ""
 	DEFAULT_REGION      = ""
-	DEFAULT_LOGFILE     = "/usr/local/var/com.github.twystd.uhppoted/logs/uhppoted-app-s3.log"
+	DEFAULT_LOGFILE     = "/usr/local/var/com.github.uhppoted/logs/uhppoted-app-s3.log"
 	DEFAULT_LOGFILESIZE = 10
 )
